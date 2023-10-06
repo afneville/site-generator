5563d397720171a5128d24c962c40018bdf452c1
Alexander Neville <git@afneville.com>
2023-10-06 10:18:34 +0100

    update index page

--- build.sh
+++ build.sh
@@ -12,12 +12,17 @@ find src -mindepth 1 \( -name '.git' -o -name 'out' -o -name 'res' -o -name 'dot
 find out -mindepth 1 -maxdepth 1 -type d | xargs -I {} cp ./redirect.html {}/index.html
 
 # build commands
+
+current_date="$(date '+%Y-%m-%d')"
+current_time="$(date '+%H:%M:%S')"
+current_commit="$(git -C ../docs rev-parse HEAD)"
+current_commit_short="$(git -C ../docs rev-parse --short HEAD)"
 command="pandoc -f markdown -t html -s"
 mathjax_flag="--mathjax=https://cdn.afneville.com/mathjax/tex-svg.js"
 constant_flags="--data-dir=$(pwd) --metadata-file=./metadata.yaml --lua-filter=md-to-html-links.lua --filter=pandoc-crossref --no-highlight"
 buildblogpost="${command} ${constant_flags} ${mathjax_flag} --shift-heading-level-by=1 --template=templates/blog-post -o"
 buildindexsection="${command} ${constant_flags} --template=templates/index-section"
-buildindex="${command} ${constant_flags} --template=templates/index-page --metadata title=index -o"
+buildindex="${command} ${constant_flags} --template=templates/index-page --metadata title=index --metadata date=${current_date} --metadata time=${current_time} --metadata commit=${current_commit} --metadata shortcommit=${current_commit_short} -o"
 
 # articles
 posts="$(find src -mindepth 2 -type f -name '*.md' | grep -v '_contents')"
