d148d74f5da0ad8fb41d2f9f201b745b3526f41a
Alexander Neville <git@afneville.com>
2024-02-14 12:05:20 +0000

    add date formatting function

--- res/onPageLoad.js
+++ res/onPageLoad.js
@@ -17,7 +17,7 @@ async function copyCode(block, copyButton) {
       para.appendChild(icon);
     },
     1000,
-    copyButton
+    copyButton,
   );
 }
 
@@ -57,5 +57,32 @@ function addAnchorLinks() {
   }
 }
 
+const nth = (d) => {
+  if (d > 3 && d < 21) return "th";
+  switch (d % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
+
+function formatDates() {
+  let publishDate = document.querySelector('.date').innerText.slice(1).split('-');
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let prettyDate = ` ${Number(publishDate[2])}${nth(Number(publishDate[2]))} ${months[Number(publishDate[1])-1]} ${publishDate[0]}`;
+  console.log(prettyDate);
+  document.querySelector('.date').innerHTML = null;
+  let icon = document.createElement("i");
+  icon.classList = "nf nf-oct-pencil";
+  // icon.style.setProperty("font-size", "0.75rem");
+  document.querySelector('.date').append(icon, prettyDate);
+}
+
+formatDates();
 addClipboardItems();
 addAnchorLinks();
